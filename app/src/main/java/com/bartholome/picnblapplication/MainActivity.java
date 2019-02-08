56e2133135990fc49896b1f458a28266a3e7cc8a
Bartholome Le Bruno <bartholome@macbook-pro-de-bartholome.home>
2019-02-08 23:43:25 +0100

    first commit

---
+++ app/src/main/java/com/bartholome/picnblapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bartholome.picnblapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
