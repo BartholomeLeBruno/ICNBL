78b80c971c43400049cf437de0b28486eb01640e
TuCao <ngtu.caong@gmail.com>
2019-02-12 22:37:53 +0100

    R in red + 8 new drawable pictures + buttons layout

--- app/src/main/java/com/bartholome/picnblapplication/MainActivity.java
+++ app/src/main/java/com/bartholome/picnblapplication/MainActivity.java
@@ -2,12 +2,22 @@ package com.bartholome.picnblapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView button, arrow;
+    TextView points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
     }
+
+
 }
