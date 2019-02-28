e1f3c58de9a30d8053f1b819db6cca0a04cb7aea
Tiventy <bartholome.lebruno@gmail.com>
2019-02-28 21:20:56 +0100

    add some test

--- app/src/test/java/com/bartholome/picnblapplication/ExampleUnitTest.java
+++ app/src/test/java/com/bartholome/picnblapplication/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.bartholome.picnblapplication;
 
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +11,41 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    private final int STATE_BLUE = 1;
+    private final int STATE_RED = 2;
+    private final int STATE_YELLOW = 3;
+    private final int STATE_GREEN = 4;
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void addition_isNotCorrect() { assertEquals(6, 2 + 2 + 2);
+    }
+
+    @Test
+    public void case_Test_With_Arrow_State_At1()
+    {
+        int state = 0;
+        int arrowState = 1;
+            switch (state) {
+                case STATE_BLUE:
+                    arrowState = STATE_BLUE;
+                    break;
+                case STATE_RED:
+                    arrowState = STATE_RED;
+                    break;
+                case STATE_YELLOW:
+                    arrowState = STATE_YELLOW;
+                    break;
+                case STATE_GREEN:
+                    arrowState = STATE_GREEN;
+                    break;
+            }
+        assertEquals(STATE_BLUE, arrowState);
+    }
+
 }
